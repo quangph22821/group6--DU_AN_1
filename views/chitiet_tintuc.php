cb69d1bdfae3850328440ccfa9492fafbc3b9480
Minhquy3112 <joshquy2003@gmail.com>
2022-11-19 01:34:16 +0700

    chi tiết các thứ

---
+++ views/chitiet_tintuc.php
@@ -0,0 +1,26 @@
+<div class="tintuc_all">
+    <div class="chitiet_tintuc">
+        <img src="./img/2.jpg" alt="">
+        <h2>Giao thoa ẩm thực Á Âu</h2>
+        <p>Ẩm thực là chiếc nôi văn hóa thể hiện rõ nét đặc trưng của 
+            mỗi vùng miền. Đặc biệt thưởng thực nghệ thuật trong cách 
+            chế biến ẩm thực Á Âu chắc chắn sẽ giúp chuyến hành trình 
+            khám phá Chân - Thiện - Mỹ của mỗi thực khách trở nên thật 
+            đáng nhớ. Thật vậy, ghé thăm bất kỳ một địa danh nào mà chưa 
+            từng nếm thử qua đặc sản của vùng đất ấy quả là một thiếu sót. 
+            The Capp Hotel với bàn tay và tâm hồn của những nghệ nhân 
+            trong gian bếp, từ những ngày đầu, vẫn luôn muốn mang lại 
+            sự tinh tế được hòa trộn bởi tinh hoa ẩm thực vào từng từng 
+            món ăn, để hành trình khám phá ẩm thực của thực khách khi đến 
+            với Hà Nội cũng sẽ trở nên vô cùng đặc sắc và đáng nhớ.
+        </p>
+    </div>
+    <div class="tintuc_lienquan">
+            <div class="lienquan_anh">
+                <a href=""><img src="./img/10.jpg" alt=""></a>
+            </div>
+            <div class="lienquan_tieude">
+                <a href=""> <h3>Những lợi ích từ Spa</h3></a>
+            </div>
+    </div>
+</div>
