4af55487746f12f4b4c14eaa1be58727d90c5b30
quangph22821 <quanghxph22821@fpt.edu.vn>
2022-11-25 16:24:54 +0700

    aaa

--- admin/loaiphong/sua.php
+++ admin/loaiphong/sua.php
@@ -1,21 +1,22 @@
+<div class="update_lp" style="">
+    <form action="index.php?act=sua&id=<?=$lp['id']?>" method='post'>
 
-<form action="index.php?act=sua&id=<?=$lp['id']?>" method='post'>
+    <input type="hidden" name="id" value="<?=$lp['id']?>">
 
-        <input type="hidden" name="id" value="<?=$lp['id']?>">
+    <div class="form-group">
+        <label for="">Tên loại phòng</label>
+        <br>
+        <input type="text" name="name_room"  value="<?= $lp['name_room']?>">
+    </div>
 
-        <div class="form-group">
-            <label for="">Tên loại phòng</label>
-            <br>
-            <input type="text" name="name_room"  value="<?= $lp['name_room']?>">
-        </div>
+    <div class="btn">
+      <input type="submit" value="Cập nhật" name="update">
+        <input type="reset" value="NHẬP LẠI">
+        <a href="index.php?act=danhsach"><input type="button" value="DANH SÁCH"></a>
+    </div>
 
-        <div class="btn">
-          <input type="submit" value="Cập nhật" name="update">
-            <input type="reset" value="NHẬP LẠI">
-            <a href="index.php?act=danhsach"><input type="button" value="DANH SÁCH"></a>
-        </div>
-        
-   
- </form>
+</form>
+
+</div>
 
 
